02440044aa5cfa83eb7ad9b330254f55a6c93cb0
Santin <santinjuancarlos@gmail.com>
2017-08-18 17:51:26 -0500

    quitar algunos estilos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,59 +10,6 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
 
         <link rel="stylesheet" href="{{asset('css/style.css')}}">
         <script src="{{asset('js/animations.js')}}"></script>
@@ -106,7 +53,6 @@
                     <li class="divnav-item-footer"> <a href="">FAQ                 </a> </li>
                 </ul>
             </div>
-            
         </footer>
 
     </body>
